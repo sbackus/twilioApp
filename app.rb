0c88a69d1081aa9e606385c63614e1e1bf593ce8
Sam Backus <sbackus@kayak.com>
2015-06-10 16:07:47 -0400

    use twilio to sent sms message

--- app.rb
+++ app.rb
@@ -1,9 +1,32 @@
 require 'sinatra'
+require 'twilio-ruby' 
+
+# put your own credentials here 
+
+ # alternatively, you can preconfigure the client like so
+Twilio.configure do |config|
+  config.account_sid = ENV['TWILIO_ACCOUNT_SID']
+  config.auth_token = ENV['TWILIO_AUTH_TOKEN']
+end
+
+configure do
+	set :twilio_client, Twilio::REST::Client.new
+end
 
 get '/' do
   erb :form
 end
 
 post '/' do
-  "send message..."
+	@number = params[:number]
+	@message = params[:message]
+	settings.twilio_client.messages.create(
+	  from: "+18454434561",
+	  to: @number,
+	  body: @message
+	)
+	erb :message_sent
 end
+
+
+ 
